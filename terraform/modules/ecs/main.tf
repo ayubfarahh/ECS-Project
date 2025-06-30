8e025180cc7887b1ca593d1a43e9b9a987042f48
ayubfarahh <ayubfarahh@gmail.com>
2025-06-30 22:21:28 +0100

    added iam role module

--- terraform/modules/ecs/main.tf
+++ terraform/modules/ecs/main.tf
@@ -1,4 +1,13 @@
 resource "aws_ecs_cluster" "ecs_cluster" {
     name = "ecs_cluster"
+}
+
+resource "aws_ecs_task_definition" "ecs_task" {
+    family = "service"
+    network_mode = "awsvpc"
+    requires_compatibilities = ["FARGATE"]
+    cpu = "256"
+    memory = "512"
+    execution_role_arn = 
   
 }
